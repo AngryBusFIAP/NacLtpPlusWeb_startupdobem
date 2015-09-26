35f3ad890fe4fe5ae28adf6a16180cd358ec686a
augribeiro <contato.augustoribeiro@hotmail.com>
2015-09-26 16:39:16 -0300

    Revert "exception"
    This reverts commit 4825841b1ed8e921c2f6605d221f8742e6c34d17.

--- src/br/com/fiap/except/Excecao.java
+++ src/br/com/fiap/except/Excecao.java
@@ -5,28 +5,4 @@ package br.com.fiap.except;
  * 26/09/15 ... 13:13.
  */
 public class Excecao extends Exception {
-	public Excecoes(String mensagem, Exception e) {
-		super(mensagem, e);
-		if (e.getClass().toString().equals
-				("class java.lang.NumberFormatException")){
-			System.out.println("Número inválido");
-		}else if (e.getClass().toString().equals
-				("class java.lang.ArithmeticException")){
-			System.out.println("Divisão por 0");
-		}else if (e.getClass().toString().equals
-				("class java.lang.NullPointerException")){
-			System.out.println("Valor inválido");
-		}else if (e.getClass().toString().equals
-				("class java.lang.FileNotFoundException")){
-			System.out.println("Arquivo não encontrado");
-		}else if (e.getClass().toString().equals
-				("class java.lang.ArrayIndexOutofBounds")){
-			System.out.println("Valor inválido");
-		}else if (e.getClass().toString().equals
-				("class java.lang.IllegalArgumentException")){
-			System.out.println("Valor inválido");
-		}
-		}this.print();
-		e.printStackTrace();
-	}
 }
