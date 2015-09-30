743e4731b7fd38c103ae4ea95073e747bcd141c3
vinylimaz <vinylimaz@live.com>
2015-09-29 22:32:30 -0300

    Adição da ultima função do ClienteDAO, o UPDATE

--- src/br/com/fiap/dao/ClienteDAO.java
+++ src/br/com/fiap/dao/ClienteDAO.java
@@ -10,15 +10,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
- * <i>Camada de CRUD</i> Created by
- * 
- * @author vinylimaz on 27/09/15 ... 05:29.
+ * <i>Camada de CRUD da tabela Cliente</i>
  *
+ * Created on 27/09/15 ... 05:29.
+ * @author vinylimaz
  * @version 0.5
  * @since 0.1
  *
  * @see br.com.fiap.beans.Cliente;
  */
+
 public class ClienteDAO {
 	private Connection c;
 
@@ -26,15 +27,15 @@ public class ClienteDAO {
         c = new ConexaoFiap().getConnection();
     }
 
-	/**
-	 * <i>Metodo para cadastro do cliente no banco de dados</i>
-	 *
-	 * @param cliente
-	 * @return String com o resultado da operação;
-	 * @throws Excecao
-	 * @author VinyLimaZ
-	 */
-	public boolean cadCliente(Cliente cliente) throws Excecao {
+    /**
+     * <i>Metodo para cadastro do cliente no banco de dados</i>
+     *
+     * @param cliente Objeto do tipo Cliente que será usado para inserir os dados na tabela T_SCN_PESSOA & T_SCN_CLIENTE
+     * @return String com o resultado da operação;
+     * @throws Excecao
+     * @author VinyLimaZ
+     */
+    public boolean cadCliente(Cliente cliente) throws Excecao {
 
 		String sql = "INSERT INTO T_SCN_PESSOA VALUES (?,?,?,?,?,?,?,?)";
 		try {
@@ -112,7 +113,31 @@ public class ClienteDAO {
 			return cli;
 		} catch (SQLException e) {
 			throw new Excecao(e);
-		}
-	}
+        }
+    }
 
+    public boolean updateCliente(Cliente cliente) throws Excecao {
+        boolean resp = false;
+        String sql = "UPDATE T_SCN_PESSOA SET NM_PESSOA = ?,  DT_NASC = ?,  TX_EMAIL = ? WHERE ID_PESSOA = ?";
+        try {
+            PreparedStatement statement = c.prepareStatement(sql);
+            statement.setString(1, cliente.getNome());
+            statement.setString(2, cliente.getDtNasc());
+            statement.setString(3, cliente.getEmail());
+            statement.setInt(4, cliente.getIdCliente());
+            if (statement.execute()) {
+                sql = "UPDATE T_SCN_CLIENTE SET NR_CPF = ?, NR_RG = ?, TX_SEXO = ?";
+                statement = c.prepareStatement(sql);
+                statement.setLong(1, cliente.getCpf());
+                statement.setString(2, cliente.getRg());
+                statement.setString(3, cliente.getSexo());
+                resp = statement.execute();
+            }
+            c.close();
+            statement.close();
+            return resp;
+        } catch (SQLException e) {
+            throw new Excecao(e);
+        }
+    }
 }
